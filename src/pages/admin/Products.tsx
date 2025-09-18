2bb4a27b2dfbfa4fc9201a311cfd803eaffee790
Shashi Madushan <156106584+Shashi-Madushan@users.noreply.github.com>
2025-09-18 21:56:37 +0530

    refactor: remove unused search icon from Products component

--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -262,9 +262,6 @@ const Products: React.FC = () => {
                                 onChange={(e) => setSearchTerm(e.target.value)}
                                 className="w-full pl-10 pr-4 py-2 bg-white border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-500 text-black"
                             />
-                            <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 text-lg">
-                                <svg width="18" height="18" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24"><circle cx="11" cy="11" r="8"/><path d="M21 21l-4.35-4.35"/></svg>
-                            </span>
                         </div>
                         {/* Filters */}
                         <div className="flex gap-3 flex-wrap">
